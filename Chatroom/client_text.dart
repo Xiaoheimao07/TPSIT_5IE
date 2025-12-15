7fbebb0c020db6c5d08eab3c1c8ba80b943f61c8
Xiaoheimao07 <davidexu07@gmail.com>
2025-12-15 10:32:30 +0100

    Aggiunta cliente testuale

---
+++ Chatroom/client_text.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+
+void main() async {
+  stdout.write("Inserisci il tuo username: ");
+  final username = stdin.readLineSync()!.trim();
+
+  final socket = await Socket.connect("127.0.0.1", 3000);
+  print("Connesso al server!");
+
+  socket.write("$username si è unito alla chat.\n");
+
+  socket.listen((data) {
+    print(utf8.decode(data).trim());
+  });
+
+
+  stdin.listen((data) {
+    final msg = utf8.decode(data).trim();
+    socket.write("$username: $msg\n");
+  });
+}
